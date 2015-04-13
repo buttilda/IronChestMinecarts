2b19eb70f970750c36fac5d4b76cade298d1534d
ganymedes01 <foka_12@hotmail.com>
2015-04-12 23:26:02 -0300

    Bump version

--- src/main/java/ganymedes01/ironchestminecarts/lib/Reference.java
+++ src/main/java/ganymedes01/ironchestminecarts/lib/Reference.java
@@ -5,5 +5,5 @@ public class Reference {
 	public static final String MOD_ID = "ironchestminecarts";
 	public static final String MOD_NAME = "Iron Chest Minecarts";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.10,);required-after:IronChest";
-	public static final String VERSION_NUMBER = "1.0.1";
+	public static final String VERSION_NUMBER = "1.0.2";
 }
